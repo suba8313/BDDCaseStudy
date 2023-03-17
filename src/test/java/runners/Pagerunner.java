9d4c447c5f6d898b3d5cb4e02c4f5252b195caf0
ASUS <ASUS@LAPTOP-1LL282FT>
2023-03-17 15:30:47 +0530

    New BDD

---
+++ src/test/java/runners/Pagerunner.java
@@ -0,0 +1,19 @@
+package runners;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+			features="src//test//resources//features//Outline.feature",
+			glue={"stepDefs"},
+			monochrome = true,
+			dryRun = false,
+			plugin= {"pretty",
+					"html:target//reports//htmlreport"}
+			)
+
+	public class Pagerunner extends AbstractTestNGCucumberTests {
+
+
+	 }
+
